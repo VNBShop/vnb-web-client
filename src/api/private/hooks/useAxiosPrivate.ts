76fe558242e9eaf21950723c178da61d06bb0993
jungjung <dungmv.now@gmail.com>
2024-02-21 21:44:24 +0700

    update axios

--- src/api/private/hooks/useAxiosPrivate.ts
+++ src/api/private/hooks/useAxiosPrivate.ts
@@ -16,6 +16,7 @@ export default function useAxiosPrivate() {
 
   useEffect(() => {
     // let isRefreshing = false
+    if (!session) return
 
     const responseIntercept = axiosPrivate.interceptors.response.use(
       (res) => res,
